8b2ee613312cb03f3d2220a644fce06b0df68359
Ovejero-Sayavedra-Mateo-Leon <mlovejerosayavedra@mail.austral.edu.ar>
2025-04-07 14:46:16 -0300

    in_string.py

--- in_string.py
+++ in_string.py
@@ -1,5 +1,13 @@
 def check_vowels():
     # Código a implementar utilizando input.
+    name = input("Ingrese su nombre: ")
+    valor = name.lower()
+    
+    print(f"Contiene a: {"a" in valor}")
+    print(f"Contiene e: {"e" in valor}")
+    print(f"Contiene i: {"i" in valor}")
+    print(f"Contiene o: {"o" in valor}")
+    print(f"Contiene u: {"u" in valor}")
 
 
 # Para verificar este ejercicio ejecutar el comando
